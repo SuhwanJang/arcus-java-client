0ee006fbc352c459a84db7693b0d3e02cae95750
hjyun <hjyun@jam2in.com>
2020-06-01 16:42:06 +0900

    TEST: add UserEntityTest

--- src/test/java/com/jam2in/arcus/admin/tool/domain/user/entity/UserEntityTest.java
+++ src/test/java/com/jam2in/arcus/admin/tool/domain/user/entity/UserEntityTest.java
@@ -1,5 +1,62 @@
 package com.jam2in.arcus.admin.tool.domain.user.entity;
 
-// TODO: do test
+import com.jam2in.arcus.admin.tool.domain.user.dto.UserDto;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+
 public class UserEntityTest {
+
+  @Test
+  public void builder() {
+    // given
+    String username = "foo";
+    String email = "foo@bar.com";
+    String password = "baz";
+    Collection<RoleEntity> roles = List.of(RoleEntity.ROLE_ADMIN, RoleEntity.ROLE_USER);
+
+    // when
+    UserEntity userEntity = UserEntity.builder()
+        .username(username)
+        .email(email)
+        .password(password)
+        .roles(roles)
+        .build();
+
+    // then
+    assertThat(userEntity.getUsername(), is(username));
+    assertThat(userEntity.getEmail(), is(email));
+    assertThat(userEntity.getPassword(), is(password));
+    assertThat(userEntity.getRoles(), is(roles));
+  }
+
+  @Test
+  public void of() {
+    // given
+    UserDto userDto = UserDto.builder()
+        .id(1L)
+        .username("foo")
+        .email("foo@bar.com")
+        .password("baz")
+        .roles(List.of(RoleEntity.ROLE_ADMIN.name(), RoleEntity.ROLE_USER.name()))
+        .build();
+
+    // when
+    UserEntity userEntity = UserEntity.of(userDto);
+
+    // then
+    assertThat(userEntity.getId(), is(not(userDto.getId())));
+    assertThat(userEntity.getUsername(), is(userDto.getUsername()));
+    assertThat(userEntity.getEmail(), is(userDto.getEmail()));
+    assertThat(userEntity.getPassword(), is(userDto.getPassword()));
+    assertThat(userEntity.getRoles(), is(userDto.getRoles().stream()
+        .map(RoleEntity::valueOf).collect(Collectors.toList())));
+  }
+
 }
