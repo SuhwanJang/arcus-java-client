06c0e98be90a74b9a50764b3db358f0bac2ffcdd
hjyun <hjyun@jam2in.com>
2020-06-02 15:31:41 +0900

    CLEANUP: remove useless request param

--- src/main/java/com/jam2in/arcus/admin/tool/domain/user/controller/UserController.java
+++ src/main/java/com/jam2in/arcus/admin/tool/domain/user/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,7 +52,7 @@ public class UserController {
 
   @GetMapping("/{id}")
   @ResponseStatus(code = HttpStatus.OK)
-  public UserDto get(@PathVariable long id, @RequestParam("page") long page) {
+  public UserDto get(@PathVariable long id) {
     return userService.get(id);
   }
 
