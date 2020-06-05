d95c48d39cbf544d508255fe7546354abc76c386
hjyun <hjyun@jam2in.com>
2020-06-05 11:26:46 +0900

    TEST: add UserDto of list test

--- src/test/java/com/jam2in/arcus/admin/tool/domain/user/dto/UserDtoTest.java
+++ src/test/java/com/jam2in/arcus/admin/tool/domain/user/dto/UserDtoTest.java
@@ -10,6 +10,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 
@@ -68,6 +71,47 @@ public class UserDtoTest {
     assertThat(userDto.getNewPassword(), is(nullValue()));
     assertThat(userDto.getRoles(), is(userEntity.getRoles().stream()
         .map(Enum::name).collect(Collectors.toList())));
+  }
+
+  @Test
+  public void of_list() {
+    // given
+    UserEntity userEntity1 = UserEntity.builder()
+        .username("foo")
+        .email("foo@bar.com")
+        .password("baz")
+        .roles(List.of(RoleEntity.ROLE_ADMIN))
+        .build();
+    ReflectionTestUtils.setField(userEntity1, "id", 1L);
+
+    UserEntity userEntity2 = UserEntity.builder()
+        .username("foofoo")
+        .email("foofoo@barbar.com")
+        .password("bazbaz")
+        .roles(List.of(RoleEntity.ROLE_USER))
+        .build();
+    ReflectionTestUtils.setField(userEntity2, "id", 2L);
+
+    // when
+    List<UserDto> userDtos = UserDto.of(List.of(userEntity1, userEntity2));
+
+    // then
+    assertThat(userDtos, contains(
+        allOf(
+            hasProperty("username", is(userEntity1.getUsername())),
+            hasProperty("email", is(userEntity1.getEmail())),
+            hasProperty("password", is(nullValue())),
+            hasProperty("roles", is(userEntity1.getRoles().stream()
+                .map(Enum::name).collect(Collectors.toList())))
+        ),
+        allOf(
+            hasProperty("username", is(userEntity2.getUsername())),
+            hasProperty("email", is(userEntity2.getEmail())),
+            hasProperty("password", is(nullValue())),
+            hasProperty("roles", is(userEntity2.getRoles().stream()
+                .map(Enum::name).collect(Collectors.toList())))
+        )
+    ));
   }
 
 }
