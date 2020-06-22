911ada23967482cab1db41f8880e7375d966d770
hjyun <hjyun@jam2in.com>
2020-06-22 18:56:42 +0900

    CLEANUP: refactor toErrorCode method

--- src/main/java/com/jam2in/arcus/admin/tool/domain/ensemble/util/ZooKeeperApiErrorUtil.java
+++ src/main/java/com/jam2in/arcus/admin/tool/domain/ensemble/util/ZooKeeperApiErrorUtil.java
@@ -19,35 +19,24 @@ import java.util.concurrent.TimeoutException;
 public final class ZooKeeperApiErrorUtil {
 
   public static ApiErrorCode toErrorCode(Throwable throwable) {
-    if (throwable instanceof TaskRejectedException) {
-      return ApiErrorCode.ZOOKEEPER_TASK_REJECTED;
-    } else if (throwable instanceof TimeoutException) {
+    if (throwable instanceof CompletionException) {
+      return toErrorCode(throwable.getCause());
+    } else if (throwable instanceof ZooKeeperException) {
+      return toErrorCode(throwable.getCause());
+    }
+
+    if (throwable instanceof TimeoutException) {
       return ApiErrorCode.ZOOKEEPER_TASK_TIMEOUT;
-    } else if (throwable instanceof CompletionException) {
-      if (throwable.getCause() instanceof TaskRejectedException) {
-        return ApiErrorCode.ZOOKEEPER_TASK_REJECTED;
-      } else if (throwable.getCause() instanceof TimeoutException) {
-        return ApiErrorCode.ZOOKEEPER_TASK_TIMEOUT;
-      } else if (throwable.getCause() instanceof ZooKeeperException) {
-        ZooKeeperException r = (ZooKeeperException) throwable.getCause();
-        if (r.getCause() instanceof ConnectException
-            || r.getCause() instanceof SocketTimeoutException) {
-          return ApiErrorCode.ZOOKEEPER_CONNECTION_FAILED;
-        } else {
-          log.info(throwable.getMessage(), throwable);
-          return ApiErrorCode.ZOOKEEPER_UNKNOWN;
-        }
-      } else if (throwable.getCause() instanceof KeeperException) {
-        if (!(throwable.getCause() instanceof KeeperException.ConnectionLossException)) {
-          log.info(throwable.getMessage(), throwable);
-        }
-        return ApiErrorCode.ZOOKEEPER_CONNECTION_FAILED;
-      } else {
-        log.info(throwable.getMessage(), throwable);
-        return ApiErrorCode.ZOOKEEPER_UNKNOWN;
-      }
+    } else if (throwable instanceof TaskRejectedException) {
+      return ApiErrorCode.ZOOKEEPER_TASK_REJECTED;
+    } else if (throwable instanceof ConnectException
+        || throwable instanceof SocketTimeoutException
+        || throwable instanceof KeeperException.ConnectionLossException) {
+      return ApiErrorCode.ZOOKEEPER_CONNECTION_FAILED;
     } else {
-      log.info(throwable.getMessage(), throwable);
+      if (throwable != null) {
+        log.error(throwable.getMessage(), throwable);
+      }
       return ApiErrorCode.ZOOKEEPER_UNKNOWN;
     }
   }
