aa98578a8b12ade3e254c6854a5172a9a0dc6c6c
hjyun <hjyun@jam2in.com>
2020-06-22 18:39:55 +0900

    FIX: add missing files

--- src/main/java/com/jam2in/arcus/admin/tool/domain/ensemble/component/ZooKeeperFourLetterComponent.java
+++ src/main/java/com/jam2in/arcus/admin/tool/domain/ensemble/component/ZooKeeperFourLetterComponent.java
@@ -1,60 +1,86 @@
 package com.jam2in.arcus.admin.tool.domain.ensemble.component;
 
-import com.google.common.net.HostAndPort;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.zookeeper.client.FourLetterWordMain;
-import org.apache.zookeeper.server.command.FourLetterCommands;
-import org.springframework.scheduling.annotation.Async;
+import com.jam2in.arcus.admin.tool.domain.ensemble.dto.ZooKeeperDto;
+import com.jam2in.arcus.admin.tool.domain.ensemble.dto.ZooKeeperFourLetterConsDto;
+import com.jam2in.arcus.admin.tool.domain.ensemble.dto.ZooKeeperFourLetterDto;
+import com.jam2in.arcus.admin.tool.domain.ensemble.dto.ZooKeeperFourLetterMntrDto;
+import com.jam2in.arcus.admin.tool.domain.ensemble.dto.ZooKeeperFourLetterSrvrDto;
+import com.jam2in.arcus.admin.tool.domain.ensemble.parser.ZooKeeperFourLetterConsParser;
+import com.jam2in.arcus.admin.tool.domain.ensemble.parser.ZooKeeperFourLetterMntrParser;
+import com.jam2in.arcus.admin.tool.domain.ensemble.parser.ZooKeeperFourLetterRuokParser;
+import com.jam2in.arcus.admin.tool.domain.ensemble.parser.ZooKeeperFourLetterSrvrParser;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Component
-@Slf4j
 public class ZooKeeperFourLetterComponent {
 
-  @SuppressWarnings("UnstableApiUsage")
-  private CompletableFuture<String> fourLetter(String address, String command,
-                                               int socketTimeoutMs) {
-    CompletableFuture<String> completableFuture = new CompletableFuture<>();
+  // TODO: use property value
+  public static final int FOUR_LETTER_SOCKET_TIMEOUT_MS = 3000;
+  public static final int FOUR_LETTER_TASK_TIMEOUT_MS = 6000;
 
+  private final ZooKeeperFourLetterAsyncComponent fourLetterComponent;
+
+  public ZooKeeperFourLetterComponent(ZooKeeperFourLetterAsyncComponent fourLetterComponent) {
+    this.fourLetterComponent = fourLetterComponent;
+  }
+
+  public ZooKeeperFourLetterSrvrDto getSrvr(String address) {
+    return fourLetterComponent.srvr(address, FOUR_LETTER_SOCKET_TIMEOUT_MS)
+        .thenApply(ZooKeeperFourLetterSrvrParser::parse)
+        .orTimeout(FOUR_LETTER_TASK_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+        .join();
+  }
+
+  public Collection<ZooKeeperFourLetterConsDto> getCons(String address) {
+    return fourLetterComponent.cons(address, FOUR_LETTER_SOCKET_TIMEOUT_MS)
+        .thenApply(ZooKeeperFourLetterConsParser::parse)
+        .orTimeout(FOUR_LETTER_TASK_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+        .join();
+  }
+
+  public ZooKeeperFourLetterMntrDto getMntr(String address) {
+    return fourLetterComponent.mntr(address, FOUR_LETTER_SOCKET_TIMEOUT_MS)
+        .thenApply(ZooKeeperFourLetterMntrParser::parse)
+        .orTimeout(FOUR_LETTER_TASK_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+        .join();
+  }
+
+  public Collection<ZooKeeperDto> getAllStats(Collection<ZooKeeperDto> zookeepers) {
+    return zookeepers.stream()
+        .map(this::getStats)
+        .collect(Collectors.toList())
+        .stream()
+        .map(CompletableFuture::join)
+        .collect(Collectors.toList());
+  }
+
+  private CompletableFuture<ZooKeeperDto> getStats(ZooKeeperDto zookeeperDto) {
     try {
-      HostAndPort hostAndPort = HostAndPort.fromString(address);
-
-      completableFuture.complete(
-          FourLetterWordMain.send4LetterWord(
-              hostAndPort.getHost(), hostAndPort.getPort(),
-              command,
-              false, socketTimeoutMs));
+      return fourLetterComponent.ruok(zookeeperDto.getAddress(), FOUR_LETTER_SOCKET_TIMEOUT_MS)
+          .thenApply(ruok ->
+              ZooKeeperFourLetterDto.builder().ruok(ZooKeeperFourLetterRuokParser.parse(ruok)))
+          .thenCombine(
+              fourLetterComponent.srvr(zookeeperDto.getAddress(), FOUR_LETTER_SOCKET_TIMEOUT_MS),
+              (builder, srvr) ->
+                  builder.srvr(ZooKeeperFourLetterSrvrParser.parse(srvr)))
+          .thenApply(builder -> {
+            zookeeperDto.setStats(builder.build());
+            return zookeeperDto;
+          })
+          .orTimeout(FOUR_LETTER_TASK_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+          .exceptionally(throwable -> {
+            zookeeperDto.setStats(ZooKeeperFourLetterDto.builder().throwable(throwable).build());
+            return zookeeperDto;
+          });
     } catch (Exception e) {
-      throw new RuntimeException(e);
+      zookeeperDto.setStats(ZooKeeperFourLetterDto.builder().throwable(e).build());
+      return CompletableFuture.completedFuture(zookeeperDto);
     }
-
-    return completableFuture;
-  }
-
-  @Async
-  public CompletableFuture<String> ruok(String address, int socketTimeoutMs) {
-    return fourLetter(address, FourLetterCommands.getCommandString(FourLetterCommands.ruokCmd),
-        socketTimeoutMs);
-  }
-
-  @Async
-  public CompletableFuture<String> srvr(String address, int socketTimeoutMs) {
-    return fourLetter(address, FourLetterCommands.getCommandString(FourLetterCommands.srvrCmd),
-        socketTimeoutMs);
-  }
-
-  @Async
-  public CompletableFuture<String> cons(String address, int socketTimeoutMs) {
-    return fourLetter(address, FourLetterCommands.getCommandString(FourLetterCommands.consCmd),
-        socketTimeoutMs);
-  }
-
-  @Async
-  public CompletableFuture<String> mntr(String address, int socketTimeoutMs) {
-    return fourLetter(address, FourLetterCommands.getCommandString(FourLetterCommands.mntrCmd),
-        socketTimeoutMs);
   }
 
 }
