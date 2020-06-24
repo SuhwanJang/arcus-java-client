36c5fce1eddba046b5a4763c40c5f9125629d773
hjyun <hjyun@jam2in.com>
2020-06-24 19:03:37 +0900

    CLEANUP: remove files

--- src/main/java/com/jam2in/arcus/admin/tool/domain/ensemble/exception/ZooKeeperException.java
+++
@@ -1,9 +0,0 @@
-package com.jam2in.arcus.admin.tool.domain.ensemble.exception;
-
-public class ZooKeeperException extends RuntimeException {
-
-  public ZooKeeperException(Throwable cause) {
-    super(cause);
-  }
-
-}
--- src/main/java/com/jam2in/arcus/admin/tool/domain/ensemble/repository/ZooKeeperRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jam2in.arcus.admin.tool.domain.ensemble.repository;
-
-import com.jam2in.arcus.admin.tool.domain.ensemble.entity.ZooKeeperEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZooKeeperRepository extends JpaRepository<ZooKeeperEntity, Long> {
-}
