d22561742e4239b70f4b243521456a953f0f0aaf
hjyun <hjyun@jam2in.com>
2020-07-09 18:51:58 +0900

    CLEANUP: reformat codes

--- src/main/java/com/jam2in/arcus/admin/tool/util/CommandSender.java
+++ src/main/java/com/jam2in/arcus/admin/tool/util/CommandSender.java
@@ -1,21 +1,3 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.jam2in.arcus.admin.tool.util;
 
 import java.io.BufferedReader;
@@ -29,25 +11,25 @@ import java.net.SocketTimeoutException;
 
 import com.google.common.net.HostAndPort;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.yetus.audience.InterfaceAudience;
 
-@InterfaceAudience.Public
 @Slf4j
 public class CommandSender {
+
   //in milliseconds, socket should connect/read within this period otherwise SocketTimeoutException
   private static final int DEFAULT_SOCKET_TIMEOUT = 5000;
 
   /**
    * Send the command
+   *
    * @param address the address(host:port)
-   * @param cmd the command string
+   * @param cmd     the command string
    * @param timeout in milliseconds, maximum time to wait while connecting/reading data
    * @return server response
-   * @throws java.io.IOException
+   * @throws java.io.IOException if a problem occurs.
    */
+  @SuppressWarnings("UnstableApiUsage")
   public static String send(String address, String cmd, int timeout)
-      throws IOException
-  {
+      throws IOException {
     try {
       HostAndPort hostAndPort = HostAndPort.fromString(address);
       return send(hostAndPort.getHost(), hostAndPort.getPort(), cmd, timeout);
@@ -56,34 +38,36 @@ public class CommandSender {
       throw e;
     }
   }
+
   /**
    * Send the command
+   *
    * @param host the destination host
    * @param port the destination port
-   * @param cmd the command string
+   * @param cmd  the command string
    * @return server response
-   * @throws java.io.IOException
+   * @throws java.io.IOException if a problem occurs.
    */
   public static String send(String host, int port, String cmd)
-      throws IOException
-  {
+      throws IOException {
     return send(host, port, cmd, DEFAULT_SOCKET_TIMEOUT);
   }
+
   /**
    * Send the command
-   * @param host the destination host
-   * @param port the destination port
-   * @param cmd the command string
+   *
+   * @param host    the destination host
+   * @param port    the destination port
+   * @param cmd     the command string
    * @param timeout in milliseconds, maximum time to wait while connecting/reading data
    * @return server response
-   * @throws java.io.IOException
+   * @throws java.io.IOException if a problem occurs.
    */
   public static String send(String host, int port, String cmd, int timeout)
-      throws IOException
-  {
+      throws IOException {
     log.info("connecting to " + host + " " + port + " cmd = " + cmd);
     Socket sock = new Socket();
-    InetSocketAddress hostaddress= host != null ? new InetSocketAddress(host, port) :
+    InetSocketAddress hostaddress = host != null ? new InetSocketAddress(host, port) :
         new InetSocketAddress(InetAddress.getByName(null), port);
     BufferedReader reader = null;
     try {
@@ -100,8 +84,8 @@ public class CommandSender {
               new InputStreamReader(sock.getInputStream()));
       StringBuilder sb = new StringBuilder();
       String line;
-      while((line = reader.readLine()) != null) {
-        sb.append(line + "\n");
+      while ((line = reader.readLine()) != null) {
+        sb.append(line).append("\n");
       }
       return sb.toString();
     } catch (SocketTimeoutException e) {
@@ -113,4 +97,5 @@ public class CommandSender {
       }
     }
   }
+
 }
