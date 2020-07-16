3db69be3cef661623ba863ce3700d4d37c5dfd8f
hjyun <hjyun@jam2in.com>
2020-07-16 17:53:28 +0900

    FIX: change to lowercase string when deserializing replication role enum type

--- src/main/java/com/jam2in/arcus/admin/tool/domain/cluster/dto/ReplicationRole.java
+++ src/main/java/com/jam2in/arcus/admin/tool/domain/cluster/dto/ReplicationRole.java
@@ -1,10 +1,13 @@
 package com.jam2in.arcus.admin.tool.domain.cluster.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang3.StringUtils;
 
 public enum ReplicationRole {
 
+  @JsonProperty("master")
   MASTER,
+  @JsonProperty("slave")
   SLAVE
   ;
 
